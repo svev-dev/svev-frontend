ea2d0ff5c4a676699e5dbf672de49bee009f8c30
Nils Petter Fremming <NFREMMING@GMAIL.COM>
2026-01-02 18:31:39 +0100

    Simplify code

--- examples/basic/main.ts
+++ examples/basic/main.ts
@@ -3,7 +3,7 @@ import { StopwatchModel } from './stopwatch/StopwatchModel';
 import { StopwatchView } from './stopwatch/StopwatchView';
 import { TodoModel } from './todo/TodoModel';
 import { TodoView } from './todo/TodoView';
-import { CheckboxView } from './form/CheckboxView';
+import { createCheckboxTest } from './form/createCheckboxTest';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
@@ -14,7 +14,7 @@ const stopwatchView = new StopwatchView(stopwatchModel);
 // Todo app
 const todoModel = new TodoModel();
 const todoView = new TodoView(todoModel);
-const checkboxView = new CheckboxView();
+const checkboxView = createCheckboxTest();
 
 // Layout for the Stopwatch and Todo app
 const layout = new Stack([stopwatchView, todoView, checkboxView]);
